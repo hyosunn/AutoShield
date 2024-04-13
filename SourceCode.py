082f139164a01db1a66706cccd58315bb6fc74ac
hyosunn <140922959+hyosunn@users.noreply.github.com>
2024-04-13 12:42:05 -0700

    Update SourceCode.py

--- SourceCode.py
+++ SourceCode.py
@@ -1,1 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import gaussian_kde
+import folium
+from folium.plugins import HeatMap
 
+autoBurglaryData = pd.read_csv()
+autoBurglaryData.sort_values(by=['Incident Date'], inplace=True)
+
+CleanedData = pd.DataFrame()
+for index, row in autoBurglaryData.head(12000).iterrows():
+    if row['Incident Subcategory'] in ['Motor Vehicle Theft', 'Larceny - From Vehicle']:
+        CleanedData = pd.concat([CleanedData, row.to_frame().transpose()], ignore_index=True)
+
+CleanedData = CleanedData.dropna(subset=['Latitude', 'Longitude'])
